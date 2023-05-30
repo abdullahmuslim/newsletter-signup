e227ac821675cbe1c852ff5f5dd39ab04bd6ca1b
Abdullah Muslim <muslimabdullah925@gmail.com>
2023-05-30 22:56:15 +0100

    uploading project files for the first time

---
+++ main.js
@@ -0,0 +1,39 @@
+const [signupForm, successMessage] = [...document.querySelectorAll("section")];
+
+const form = document.querySelector("form");
+form.addEventListener("submit", validate);
+
+const input = document.querySelector("input");
+input.addEventListener("input", closeError);
+
+const button = document.getElementById("closeMessage");
+button.addEventListener("click", closeSuccessMessage);
+
+function validate(e){
+  e.preventDefault();
+  const el = e.currentTarget;
+  const email = el.children[2].value;
+  (email.match(/^.+@[a-zA-Z]+.*\.com$/))? openSuccessMessage() : showError();
+}
+
+function showError(){
+  const error = document.querySelector(".error");
+  error.textContent = "Valid email required";
+}
+
+function closeError(){
+  const error = document.querySelector(".error");
+  error.textContent = "";
+}
+
+function openSuccessMessage(){
+  const insertedEmail = document.getElementById("insertedEmail");
+  insertedEmail.textContent = input.value;
+  signupForm.style.display = "none";
+  successMessage.style.display = "flex";
+}
+
+function closeSuccessMessage(){
+  signupForm.style.display = "flex";
+  successMessage.style.display = "none";
+}
